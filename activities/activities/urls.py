234a603632e413d4b0fabba018b60e8564e068b8
Vishnu Jayan <vishnujayadhevan@yahoo.com>
2020-04-28 23:04:54 +0530

    mdofified the URL

--- activities/activities/urls.py
+++ activities/activities/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import activity.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', activity.views.index),
     path('activity/', include('activity.urls') )
 ]
